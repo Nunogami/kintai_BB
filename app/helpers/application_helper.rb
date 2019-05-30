05ed73074b3a7152fe6ca679c6fa6b571bf615b1
nunogami <nuno3434@yahoo.co.jp>
2019-05-30 03:23:35 +0000

    Finish static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページごとにタイトルを返す。
+  def full_title(page_title = "") # メソッド名と引数の定義
+    base_title = "勤怠管理システム" # 変数に代入
+    if page_title.empty? # 引数の論理値確認
+      base_title # page_titleが空（""）の時は既に定義してあるbase_titleを返す
+    else
+      page_title + " | " + base_title # 文字列を結合して返す
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root 'application#hello'
+  root 'static_pages#home'
 end
