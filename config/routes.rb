e1708b65497364b850c5ac1123cb78533b02e154
nunogami <nuno3434@yahoo.co.jp>
2019-05-31 11:24:40 +0000

    basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  '/signup', to: 'users#new'
+  get  '/signup',   to: 'users#new'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
