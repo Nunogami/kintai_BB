fff434295b05772ecd736f681f54f367aa213a0e
nunogami <nuno3434@yahoo.co.jp>
2019-06-09 04:43:49 +0000

    Additional function 8 completed

--- db/migrate/20190607090847_add_column_to_users.rb
+++ db/migrate/20190607090847_add_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :admin, :boolean, default: false
+    # add_column :users, :admin, :boolean, default: false
   end
 end
